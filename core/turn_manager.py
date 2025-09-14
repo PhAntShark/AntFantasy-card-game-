deafaac4536e88a53c5fd3475dbe876170084051
PhAntShark <accmine82@gmail.com>
2025-09-14 23:38:20 +0800

    add turn manager

---
+++ core/turn_manager.py
@@ -0,0 +1,23 @@
+from player import Player
+
+class TurnManager:
+    def __init__(self, players:list [Player]):
+        self.players = players
+        self.current_player_index = 0
+        self.turn_count = 1   
+
+    def current_player(self):
+        return self.players[self.current_player_index]
+    
+    def start_turn(self):
+        print(f'Turn {self.turn_count}  Start: Player{self.current_player().player_index}')
+        
+    def end_turn(self):
+        print(f" Player {self.current_player().player_index} Turn Ends ")
+        self.next_turn()
+            
+    def next_turn(self):
+        self.current_player_index = (self.current_player_index +1 ) % len(self.players)
+        if self.current_player_index == 0:
+            self.turn_count +=1  
+            print(f'turn {self.turn_count}')
