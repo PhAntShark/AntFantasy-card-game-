58bdb18e41e02c7a03f1b844fc7c327fa23bd97d
PhAntShark <accmine82@gmail.com>
2025-09-15 17:32:26 +0800

    add summon to player attribute

--- core/player.py
+++ core/player.py
@@ -2,20 +2,19 @@ from card import Card
 
 
 class Player:
-    # held_card = 0
-    # field = 0
-    def __init__( self, held_cards, grave_yard, field_cards, player_index, life_points=5000, name = None):  # argument MUST front of parameter
+    def __init__( self, held_cards, grave_yard, field_cards, player_index, life_points=5000, name = None, player_summon = False):  # argument MUST front of parameter
         self.player_index = player_index
         self.life_points = life_points
         self.held_cards = []
         self.grave_yard = []
         self.field_cards = []
+        self.player_summon = player_summon
         self.name = name if name else f'Player {player_index}'
+        
     def draw_card(self, card):
         if len(self.held_cards) <= 15:
             self.held_cards.append(card)
-            self.held_cards += 1
-            # print('you draw the card')
+            print('you draw the card')
         else:
             return "your card on hand is full"
 
@@ -29,9 +28,8 @@ class Player:
     def card_field_summon(self, card):
         if len(self.field_cards) < 10:  # if field < 10
             self.field_cards.append(card)  # append the card from the held_Card list
-            # field += 1  # field = +1
             self.held_cards.remove(card)  # pop the card from the list
-            # held_card -= 1  # held_card = -1
+            self.player_summon = True
             return f"success for summon {card} on the field"
         else:
             return "your field is full"
