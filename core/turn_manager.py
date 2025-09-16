533bf49835b16e3aab19d9f019085085f4dd8bbc
PhAntShark <accmine82@gmail.com>
2025-09-16 22:15:26 +0800

    adjust turn manager 1
    add opponent player

--- core/turn_manager.py
+++ core/turn_manager.py
@@ -9,13 +9,17 @@ class TurnManager:
     def current_player(self):
         return self.players[self.current_player_index]
     
+    def opponent_player(self):
+        other_index = (self.current_player_index +1 ) % len(self.players)
+        return self.players[other_index]
+    
     def start_turn(self):
         print(f'Turn {self.turn_count}  Start: Player{self.current_player().player_index}')
         
     def end_turn(self):
         print(f" Player {self.current_player().player_index} Turn Ends ")
         self.next_turn()
-            
+        
     def next_turn(self):
         self.current_player_index = (self.current_player_index +1 ) % len(self.players)
         if self.current_player_index == 0:
